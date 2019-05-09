ff11d5ac495c7d7275fd3521068ac0306f68730b
Hudson <hmcashan13@gmail.com>
2019-05-09 18:31:02 -0500

    added facebook login button api

--- app/src/main/java/com/example/hudsonmcashan/guradianaangel/Login.kt
+++ app/src/main/java/com/example/hudsonmcashan/guradianaangel/Login.kt
@@ -4,13 +4,24 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
-
+    val EMAIL = "email"
+    val PUBLIC_PROFILE = "public_profile"
+    val USER_PERMISSION = "user_friends"
     override fun onCreate(savedInstanceState: Bundle?) {
+        var callbackManager: CallbackManager? = null
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
@@ -25,7 +36,27 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        callbackManager = CallbackManager.Factory.create()
+        facebook_login_button.setOnClickListener {
+            facebook_login_button.setReadPermissions(Arrays.asList(EMAIL, PUBLIC_PROFILE, USER_PERMISSION))
+            facebook_login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(exception: FacebookException) {
+
+                }
+            })
+        }
+
     }
+
     private fun performLogin() {
         val email = email_editText_login.text.toString()
         val password = password_editText_login.text.toString()
--- app/src/main/java/com/example/hudsonmcashan/guradianaangel/SpashActivity.kt
+++ app/src/main/java/com/example/hudsonmcashan/guradianaangel/SpashActivity.kt
@@ -3,10 +3,16 @@ package com.example.hudsonmcashan.guradianaangel
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+
         val intent = Intent(this, DeviceActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
