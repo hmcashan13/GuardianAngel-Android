699f97a53ef9a21418d7243e7e2b4208e6ce08b4
Hudson <hmcashan13@gmail.com>
2019-06-06 22:52:06 -0500

    temp settings not persisting

---
+++ app/src/main/java/com/example/hudsonmcashan/guradianaangel/Preferences.kt
@@ -0,0 +1,25 @@
+package com.example.hudsonmcashan.guradianaangel
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Prefs (context: Context) {
+    val PREFS_FILENAME = "com.example.hudsonmcashan.guradianaangel"
+    val TEMP_SENSOR_ON_OFF = "tempSensorOnOff"
+    val FARENHEIT_CELSIUS = "farenheitCelsius"
+    val MAX_TEMP = "maxTemp"
+
+    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0);
+
+    var tempSensorOnOff: Boolean
+        get() = prefs.getBoolean(TEMP_SENSOR_ON_OFF,true)
+        set(value) = prefs.edit().putBoolean(TEMP_SENSOR_ON_OFF, value).apply()
+
+    var farenheitCelsius: Boolean
+        get() = prefs.getBoolean(TEMP_SENSOR_ON_OFF,true)
+        set(value) = prefs.edit().putBoolean(FARENHEIT_CELSIUS, value).apply()
+
+    var maxTemp: Int
+        get() = prefs.getInt(TEMP_SENSOR_ON_OFF,90)
+        set(value) = prefs.edit().putInt(MAX_TEMP, value).apply()
+}
--- app/src/main/java/com/example/hudsonmcashan/guradianaangel/TemperatureAdjustActivity.kt
+++ app/src/main/java/com/example/hudsonmcashan/guradianaangel/TemperatureAdjustActivity.kt
@@ -2,17 +2,25 @@ package com.example.hudsonmcashan.guradianaangel
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_temperature.*
-import kotlinx.android.synthetic.main.activity_temperature.view.*
 
 class TemperatureAdjustActivity : AppCompatActivity() {
+    var prefs: Prefs? = null
+    var tempSensor: Boolean? = null
+    var farenheitCelsius: Boolean? = null
+    var maxTemp: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temperature)
 
         setupActionBar()
-        setupSwitches()
+        setupPrefs()
+        setupTemperatureSensorOnOffSwitch()
+        setupFarenheitCelsiusSwitch()
+        setupMaxTemperatureSlider()
     }
 
     private fun setupActionBar() {
@@ -23,15 +31,88 @@ class TemperatureAdjustActivity : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
     }
 
+    // Sets up the back button
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
-    private fun setupSwitches() {
-        celsiusOrFarenheit_switch.setOnCheckedChangeListener { buttonView, isChecked ->
-            celsiusOrFarenheit_status.textView.text = if (isChecked)  "°F" else "°C"
-        }
-        // TODO: setup switch to turn on/off temperature sensor
+    private fun setupPrefs() {
+        prefs = Prefs(this)
     }
+
+    private fun setupTemperatureSensorOnOffSwitch() {
+        tempSensor = prefs?.tempSensorOnOff ?: true
+        when(tempSensor) {
+            true -> tempSensorOnOff_status.text = "On"
+            false -> tempSensorOnOff_status.text = "Off"
+        }
+        tempSensorOnOff_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            val message = if (isChecked)  "On" else "Off"
+            tempSensorOnOff_status.text = message
+            Toast.makeText(this@TemperatureAdjustActivity, "Temperature Sensor is now $message", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupFarenheitCelsiusSwitch() {
+        farenheitCelsius = prefs?.farenheitCelsius ?: true
+        when(farenheitCelsius) {
+            true -> farenheitCelsius_status.text = "°F"
+            false -> farenheitCelsius_status.text = "°C"
+        }
+        farenheitCelsius_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            farenheitCelsius_status.text = if (isChecked)  "°F" else "°C"
+            if (isChecked) {
+                Toast.makeText(this@TemperatureAdjustActivity, "Murica!!!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@TemperatureAdjustActivity, "You are now using the metric system", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+    }
+
+    private fun setupMaxTemperatureSlider() {
+        maxTemp = prefs?.maxTemp ?: 90
+        if (farenheitCelsius != null && farenheitCelsius as Boolean) {
+            maxTemp_status.text = "${maxTemp}°F"
+        } else {
+            val celsiusTemp = (maxTemp!! - 32) * 5/9
+            maxTemp_status.text = "${celsiusTemp}°C"
+        }
+
+        maxTemp_slider?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                val newMaxTemp = seekBar.progress + 80
+                if (farenheitCelsius != null && farenheitCelsius as Boolean) {
+                    maxTemp_status.text = "${newMaxTemp}°F"
+                } else {
+                    val celsiusTemp = (newMaxTemp - 32) * 5/9
+                    maxTemp_status.text = "${celsiusTemp}°C"
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+                val newMaxTemp = seekBar.progress + 80
+                prefs?.maxTemp = newMaxTemp
+                if (farenheitCelsius != null && farenheitCelsius as Boolean) {
+                    Toast.makeText(this@TemperatureAdjustActivity, "Max Temperature is now $newMaxTemp°F", Toast.LENGTH_SHORT).show()
+                } else {
+                    val celsiusTemp = (newMaxTemp - 32) * 5/9
+                    Toast.makeText(this@TemperatureAdjustActivity, "Max Temperature is now $celsiusTemp°C", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        })
+
+    }
+
 }
+
+
